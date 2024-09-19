9c7c3a706b320f5264acbf57a711d2b657dcad6a
mjlynch <micheal.j.lynch@gmail.com>
2024-09-19 09:32:40 -0400

    catching client casestudy up to lab 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
+// how to reference other modules
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
+// decorator - tells Angular we are building a component in typescript
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-casestudy', // declares html tag that contains the app (i.e. <app-exercises></app-exercises>)
+	templateUrl: './app.component.html', // specifies html to be used for component
+	styleUrl: './app.component.scss', // specifies styling to be used for component (different than styles.scss which is site-wide)
 })
+
+// defines typescript class AppComponent
+// export keyword indicates this module can be imported into other modules
 export class AppComponent {
-  title = 'INFO5059-clientcasestudy';
+	title: string = '';
+
+	constructor(private location: Location) {
+		let path = location.path();
+
+		if (path && path.length > 1) {
+			let header = path.substring(1,2).toUpperCase();
+			header += path.substring(2);
+			this.setTitle(header);
+		}
+		else if (path === '') {
+			this.setTitle('');
+		}
+	}
+
+	setTitle(header: string) {
+		this.title = header ? header : 'Home';
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
+// modules
 import { AppRoutingModule } from './app-routing.module';
+import { MatComponentsModule } from './mat-components/mat-components.module';
+
+// components
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatComponentsModule
+	],
+	providers: [
+		provideClientHydration(),
+		provideAnimationsAsync(),
+		provideHttpClient(withFetch())
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
